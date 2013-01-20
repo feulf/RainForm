ea0dcc26f1142be5b45bca8ffa363e38b44ba4d6
Rain <rainelemental@gmail.com>
2013-01-19 23:33:12 -0500

    added library Rain\Form

---
+++ library/Rain/Form.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Rain;
+
+/**
+ * Form - Form generator class
+ * 
+ * @package Rain
+ * @version 3
+ * @author Federico Ulfo
+ * @link http://rainframework.com
+ */
+
+/**
+ * Form is a form generator class
+ */
+class Form {
+
+}
